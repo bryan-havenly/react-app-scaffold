412806eb231e995d7c421be266ed8b2e94eab6a0
Jordan Hammond <jahammo2@gmail.com>
2018-11-27 17:03:15 -0700

    added default query resolvers

--- historyRoutingBased/lib/queryResolvers.js
+++ historyRoutingBased/lib/queryResolvers.js
@@ -1,1 +1,14 @@
-export default {};
+import number   from 'shared/lib/queryResolvers/number';
+
+/*
+  this object is used in both the initialState and the history interface to determine what a
+  valid state for a query is. The resolvers basically:
+
+  - Return null, or an empty array, for blank or undefined values
+  - Return normalized versions of their input otherwise
+*/
+
+export default {
+  pageNumber          : number,
+  pageSize            : number,
+};
